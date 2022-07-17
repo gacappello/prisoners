2b38a3ec259e05290b40861f13f08d0d3726d42b
Gio <gioalb@mail.com>
2022-07-17 18:04:46 +0200

    Changed logic

--- src/logic.c
+++ src/logic.c
@@ -1,16 +1,17 @@
+#include <stdlib.h>
+
 #include "../includes/logic.h"
 #include "../includes/game.h"
 #include "../includes/prisoner.h"
 #include "../includes/box.h"
-#include <stdlib.h>
 
-int make_choice(prisoner_t prisoner, box_t boxes[], game_state_t *state){
+int make_choice(prisoner_t prisoner, box_t boxes[], box_t path[], game_state_t *state){
     switch(state->strategy){
         case LOOPY:
-            return loopy_strat(prisoner, boxes, prisoner.num - OFFSET, TRIES_LIMIT);
+            return loopy_strat(prisoner, boxes, path, prisoner.num, TRIES_LIMIT);
             break;
         case RANDOMNESS:
-            return rand_strat(prisoner, boxes, TRIES_LIMIT);
+            return rand_strat(prisoner, boxes, path, TRIES_LIMIT);
             break;
         default:
             return 0;
@@ -20,36 +21,48 @@ int make_choice(prisoner_t prisoner, box_t boxes[], game_state_t *state){
     return 0;
 }
 
-int loopy_strat(prisoner_t prisoner, box_t boxes[], int searching_in, int tries_left){
-    // TODO: List all opened boxes
+int loopy_strat(prisoner_t prisoner, box_t boxes[], box_t path[], int searching_in, int tries_left){
     tries_left--;
     if(tries_left == 0){
         return 0;
     }
 
     int paper = boxes[searching_in - OFFSET].paper;
-    int num   = prisoner.num; 
+    int num   = prisoner.num;
+
+    boxes[searching_in].opened = 1;
+    path[TRIES_LIMIT - tries_left - 1] = boxes[searching_in];
+
+    // Found
     if(paper == num){
         return 1;
     }
 
-    return loopy_strat(prisoner, boxes, paper, tries_left);
+    // Not found
+    return loopy_strat(prisoner, boxes, path, paper, tries_left);
 }
 
-int rand_strat(prisoner_t prisoner, box_t *boxes, int tries_left){
-    // TODO: List all opened boxes
+int rand_strat(prisoner_t prisoner, box_t boxes[], box_t path[], int tries_left){
     tries_left--;
     if(tries_left == 0){
         return 0;
     }
     
-    // TODO: Filter opened boxes
-    int rand_index = rand() % NUMBER;
-    int paper = boxes[rand_index].paper;
-    int num = prisoner.num;
+    // Checks if box is opened
+    do{
+        int rand_index = rand() % NUMBER;
+        int paper = boxes[rand_index].paper;
+        int num = prisoner.num;
+    }while(boxes[rand_index].opened);
+
+    boxes[searching_in].opened = 1;
+    path[TRIES_LIMIT - tries_left - 1] = boxes[rand_index];
+
+    // Found
     if (paper == num){
         return 1;
     }
 
-    return rand_strat(prisoner, boxes, tries_left);
+    // Not found
+    return rand_strat(prisoner, boxes, path, tries_left);
 }
